a47b1fb5e2ed21c67cf13e9122de6d5a41923c31
Paul <pldobbs@mac.com>
2018-05-16 13:31:45 -0500

    native lab

--- screens/ProjectDetail.js
+++ screens/ProjectDetail.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import { ScrollView, Text } from "react-native";
 
 export default class ProjectDetail extends Component {
-  static navigationOptions = ({ navigation }) => ({
-    title: navigation.state.params.data.name
-  });
+  // static navigationOptions = ({ navigation }) => ({
+  //   title: navigation.state.params.data.name
+  // });
   constructor(props) {
     super(props);
     this.pizza = this.props.navigation.state.params.data;
@@ -12,6 +12,8 @@ export default class ProjectDetail extends Component {
   render() {
     return (
       <ScrollView>
+        <Text>{this.pizza.name}</Text>
+        <Text />
         <Text>{this.pizza.description}</Text>
       </ScrollView>
     );
--- screens/ProjectStudents.js
+++ screens/ProjectStudents.js
@@ -2,10 +2,37 @@ import React, { Component } from "react";
 import { ScrollView, Text } from "react-native";
 
 export default class ProjectStudents extends Component {
+  // static navigationOptions = ({ navigation }) => ({
+  //   title: navigation.state.params.data.name
+  // });
+  constructor(props) {
+    super(props);
+    this.state = {
+      students: []
+    };
+    this.project = this.props.navigation.state.params.data;
+  }
+  async componentWillMount() {
+    try {
+      let results = await fetch(
+        `https://gravity.covalence.io/api/graduation/teams/${this.project.id}`
+      );
+      let students = await results.json();
+      this.setState({ students });
+    } catch (err) {
+      console.log(err);
+    }
+  }
   render() {
     return (
       <ScrollView>
-        <Text>Sum Students n Stuff</Text>
+        {this.state.students.map((student, index) => {
+          return (
+            <Text key={index}>
+              {student.firstname} {student.lastname}
+            </Text>
+          );
+        })}
       </ScrollView>
     );
   }
